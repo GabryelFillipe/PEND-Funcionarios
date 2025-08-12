89c36d48b5c3b81743724db4db3cdcafd71f3084
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-08-12 10:17:16 -0300

    Criando função responsavel por atribuir e carregar os funcionarios

--- app.js
+++ app.js
@@ -2,3 +2,27 @@
 
 import funcionarios from "./funcionarios.json" with{type: "json"}
 
+function criarFuncionario(funcionarios){
+
+    const containerFuncionairos = document.getElementById('funcionarios')
+    const card = document.createElement('div')
+    
+    const imagemFuncionario = document.createElement('img')
+    imagemFuncionario.src = funcionarios.imagem
+
+    const nomeFuncionario = document.createElement('p')
+    nomeFuncionario.textContent = funcionarios.nome
+
+    const cargoFuncionario = document.createElement('span')
+    cargoFuncionario.textContent = funcionarios.cargo
+
+    containerFuncionairos.appendChild(card)
+    card.appendChild(imagemFuncionario)
+    card.appendChild(nomeFuncionario)
+    card.appendChild(cargoFuncionario)
+}
+function carregarFuncionarios(){
+    funcionarios.forEach(criarFuncionario)
+}
+
+carregarFuncionarios()
